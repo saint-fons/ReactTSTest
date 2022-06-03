dc899616eab9fb04d2a2f5c0ebd511a220c9a4dd
Ivan Zhuravlev <i.zhuravlev.work@gmail.com>
2022-06-03 13:27:15 +0300

    Добавлены хуки из реакт хукс

---
+++ my-app/src/ReactTSHooks/UseImageOnLoad.tsx
@@ -0,0 +1,42 @@
+import React, { CSSProperties } from "react";
+import { useImageOnLoad } from "usehooks-ts";
+
+const UseImageOnLoad = () => {
+  const { handleImageOnLoad, css } = useImageOnLoad();
+
+  const style: { [key: string]: CSSProperties } = {
+    wrap: {
+      position: "relative",
+      width: 400,
+      height: 400,
+      margin: "auto",
+    },
+    image: {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: `100%`,
+      height: `100%`,
+    },
+  };
+
+  return (
+    <div style={style.wrap}>
+      {/* Small image load fast */}
+      <img
+        style={{ ...style.image, ...css.thumbnail }}
+        src="https://via.placeholder.com/150"
+        alt="thumbnail"
+      />
+      {/* Full size image */}
+      <img
+        onLoad={handleImageOnLoad}
+        style={{ ...style.image, ...css.fullSize }}
+        src="https://via.placeholder.com/600"
+        alt="fullImage"
+      />
+    </div>
+  );
+};
+
+export default UseImageOnLoad;
--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ContextHook from "./Hooks/ContextProvider";
+import ContextHook from "./MainHooks/ContextProvider";
+import BooleanHook from "./ReactTSHooks/BooleanHook";
+import Counter from "./ReactTSHooks/CounterButtons";
+import UseImageOnLoad from "./ReactTSHooks/UseImageOnLoad";
 
 ReactDOM.render(
   <React.StrictMode>
     <ContextHook />
+    <BooleanHook />
+    <Counter />
+    <UseImageOnLoad />
   </React.StrictMode>,
   document.getElementById("root")
 );
