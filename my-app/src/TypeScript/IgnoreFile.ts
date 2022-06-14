ead0b5cf3d6cf92dfefbf8388d86483f2406901e
Ivan Zhuravlev <izhuravlev@conteq.ru>
2022-06-14 11:58:01 +0300

    Игноирование файла в инлайн стиле

--- my-app/src/TypeScript/IgnoreFile.ts
+++ my-app/src/TypeScript/IgnoreFile.ts
@@ -1,6 +1,6 @@
 export let wrongString: string =
   "This variable with Typescript Error";
 
-/* // @ts-ignore */
+// @ts-ignore
 wrongString = 10;
 //Пример исключенияя ts файла. Ошибка в vs коде, но компилируется без ошибок
